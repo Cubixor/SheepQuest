0a201b478fef33ba9636e0e49fca20c555dba941
Cubixor <kubamarybki@gmail.com>
2024-09-03 19:37:18 +0200

    Add items

---
+++ src/main/java/me/cubixor/sheepquest/items/TeamItem.java
@@ -0,0 +1,19 @@
+package me.cubixor.sheepquest.items;
+
+import me.cubixor.minigamesapi.spigot.game.ArenasManager;
+import me.cubixor.minigamesapi.spigot.game.arena.LocalArena;
+import me.cubixor.minigamesapi.spigot.game.items.ClickableItem;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class TeamItem extends ClickableItem {
+
+    public TeamItem() {
+        super("items.team-choose-item", "game.team-item-name", "game.team-item-lore");
+    }
+
+    @Override
+    public void handleClick(ArenasManager arenasManager, LocalArena localArena, Player player, PlayerInteractEvent evt) {
+
+    }
+}
