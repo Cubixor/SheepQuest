c891b79d576a57410adb5c506e4879ff432360c4
Cubixor <kubamarybki@gmail.com>
2024-09-06 21:16:54 +0200

    Register kit listeners

--- src/main/java/me/cubixor/sheepquest/game/kits/KitManager.java
+++ src/main/java/me/cubixor/sheepquest/game/kits/KitManager.java
@@ -1,19 +1,29 @@
 package me.cubixor.sheepquest.game.kits;
 
+import me.cubixor.minigamesapi.spigot.MinigamesAPI;
 import me.cubixor.minigamesapi.spigot.game.ArenasRegistry;
 import me.cubixor.sheepquest.game.SheepPickupHandler;
+import org.bukkit.Bukkit;
 
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.Map;
 
 public class KitManager {
 
-    private final Map<KitType, Kit> kits = new HashMap<>();
+    private final Map<KitType, Kit> kits = new EnumMap<>(KitType.class);
 
     public KitManager(ArenasRegistry arenasRegistry, SheepPickupHandler sheepPickupHandler) {
-        kits.put(KitType.STANDARD, new KitStandard(arenasRegistry));
-        kits.put(KitType.ARCHER, new KitArcher(arenasRegistry));
-        kits.put(KitType.ATHLETE, new KitAthlete(arenasRegistry, sheepPickupHandler));
+        KitStandard kitStandard = new KitStandard(arenasRegistry);
+        KitArcher kitArcher = new KitArcher(arenasRegistry);
+        KitAthlete kitAthlete = new KitAthlete(arenasRegistry, sheepPickupHandler);
+
+        kits.put(KitType.STANDARD, kitStandard);
+        kits.put(KitType.ARCHER, kitArcher);
+        kits.put(KitType.ATHLETE, kitAthlete);
+
+        Bukkit.getPluginManager().registerEvents(kitStandard, MinigamesAPI.getPlugin());
+        Bukkit.getPluginManager().registerEvents(kitArcher, MinigamesAPI.getPlugin());
+        Bukkit.getPluginManager().registerEvents(kitAthlete, MinigamesAPI.getPlugin());
     }
 
     public Map<KitType, Kit> getKits() {
