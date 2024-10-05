02747e1e88f66034afd0d646b57174a98ca93a1e
Cubixor <kubamarybki@gmail.com>
2024-10-05 22:35:25 +0200

    Add an option to disable bossbar

--- src/main/java/me/cubixor/sheepquest/game/BossBarManager.java
+++ src/main/java/me/cubixor/sheepquest/game/BossBarManager.java
@@ -1,5 +1,6 @@
 package me.cubixor.sheepquest.game;
 
+import me.cubixor.minigamesapi.spigot.MinigamesAPI;
 import me.cubixor.minigamesapi.spigot.utils.Messages;
 import me.cubixor.sheepquest.arena.Team;
 import me.cubixor.sheepquest.game.kits.KitType;
@@ -7,6 +8,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 import java.util.EnumMap;
@@ -14,28 +16,42 @@ import java.util.Map;
 
 public class BossBarManager {
 
+    private final boolean teamBossBarEnabled;
+    private final boolean kitBossBarEnabled;
     private final Map<Team, BossBar> teamBossBars = new EnumMap<>(Team.class);
     private final Map<KitType, BossBar> kitBossBars = new EnumMap<>(KitType.class);
 
     public BossBarManager() {
-        for (Team team : Team.values()) {
-            BossBar bossBar = Bukkit.createBossBar(Messages.get("game.bossbar-team", "%team%", team.getName()), team.getBarColor(), BarStyle.SOLID);
-            teamBossBars.put(team, bossBar);
+        FileConfiguration config = MinigamesAPI.getPlugin().getConfig();
+        teamBossBarEnabled = config.getBoolean("team-bossbar");
+        kitBossBarEnabled = config.getBoolean("kit-bossbar");
+
+        if (teamBossBarEnabled) {
+            for (Team team : Team.values()) {
+                BossBar bossBar = Bukkit.createBossBar(Messages.get("game.bossbar-team", "%team%", team.getName()), team.getBarColor(), BarStyle.SOLID);
+                teamBossBars.put(team, bossBar);
+            }
         }
 
-        for (KitType kitType : KitType.values()) {
-            BossBar bossBar = Bukkit.createBossBar(Messages.get("kits.bossbar-kit", "%kit%", kitType.getName()), BarColor.WHITE, BarStyle.SOLID);
-            bossBar.setProgress(0);
-            kitBossBars.put(kitType, bossBar);
+        if (kitBossBarEnabled) {
+            for (KitType kitType : KitType.values()) {
+                BossBar bossBar = Bukkit.createBossBar(Messages.get("kits.bossbar-kit", "%kit%", kitType.getName()), BarColor.WHITE, BarStyle.SOLID);
+                bossBar.setProgress(0);
+                kitBossBars.put(kitType, bossBar);
+            }
         }
     }
 
     public void addTeamBossBar(Player player, Team team) {
+        if (!teamBossBarEnabled) return;
+
         removeTeamBossBar(player);
         teamBossBars.get(team).addPlayer(player);
     }
 
     public void removeTeamBossBar(Player player) {
+        if (!teamBossBarEnabled) return;
+
         for (BossBar bossBar : teamBossBars.values()) {
             if (bossBar.getPlayers().contains(player)) {
                 bossBar.removePlayer(player);
@@ -44,11 +60,15 @@ public class BossBarManager {
     }
 
     public void addKitBossBar(Player player, KitType kitType) {
+        if (!kitBossBarEnabled) return;
+
         removeKitBossBar(player);
         kitBossBars.get(kitType).addPlayer(player);
     }
 
     public void removeKitBossBar(Player player) {
+        if (!kitBossBarEnabled) return;
+
         for (BossBar bossBar : kitBossBars.values()) {
             if (bossBar.getPlayers().contains(player)) {
                 bossBar.removePlayer(player);
